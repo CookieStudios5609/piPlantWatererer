971332dda21e12b45b90a98817b42f921e6db20b
2600 <78653311+CookieStudios5609@users.noreply.github.com>
2022-07-08 20:58:39 -0400

    [Feat] Add staticfile support, websocket, and root redirect
    This commit adds a websocket to update the reading from the moisture sensor every second. To display this, I created a basic website. The root redirects to it, which is hosted using the StaticFiles mount.

--- main.py
+++ main.py
@@ -1,18 +1,43 @@
-from fastapi import FastAPI, status, Response
+from fastapi import FastAPI, WebSocket, staticfiles, WebSocketDisconnect, responses
 from sensor import SensorController
 import pump
+import asyncio
+
 
 
 app = FastAPI()
+sensor = SensorController()
 
 
 @app.on_event("startup")
 async def start():
-    sensor = SensorController()
+
     pump.init_gpio()
+    app.mount("/", staticfiles.StaticFiles(directory="static", html=True))
+
+
+@app.websocket("/read_live")
+async def live_moisture_data(websocket: WebSocket):
+    await websocket.accept()
+    try:
+        print("Connected to websocket")
+        while True:
+            await websocket.send_text(str(sensor.read_sensor()))
+            await asyncio.sleep(1)
+    except WebSocketDisconnect:
+        print("Connection to websocket closed")
 
 
 @app.get("/")
-async def root():
-    return status.HTTP_200_OK
+async def redirect_to_main():
+    return responses.RedirectResponse(url="/watererer.html")
 
+
+@app.get("/read_sensor")
+async def get_current_moisture():
+    return sensor.read_sensor()
+
+
+@app.on_event("shutdown")
+async def close():
+    pump.disable_pin()
